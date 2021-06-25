b3f8166e86b27f77d7d24c909f1eaa03b3ca27c8
metonymize-kripa <kripa@metonymize.com>
2021-06-25 11:45:45 -0500

    toy model to price option assuming cauchy distribution

---
+++ vanilla_option_pricer.py
@@ -0,0 +1,26 @@
+import math, random, numpy, scipy
+from scipy.stats import cauchy, norm
+
+def genCauchySpotPriceSample(spot,days_ahead,loc,scale):
+    running_sum_spot=spot
+    cauchy_returns_trace = cauchy.rvs(loc,scale,days_ahead)
+    for cauchy_return_sample in cauchy_returns_trace:
+        running_sum_spot += running_sum_spot*cauchy_return_sample
+    return running_sum_spot
+
+##Simple Monte Carlo Pricing Vanilla Call Option using Cauchy Distribution, and 0 interest rate
+def genCauchyCallOptionPrice(days_ahead, strike, spot, loc, scale, num_traces):
+    running_sum_call_payoff = 0
+    ##Simulate spot price traces and corresponding pay offs
+    for i in range(num_traces):
+        this_spot = genCauchySpotPriceSample(spot,days_ahead,loc,scale)
+        #Determine payoff of this specific path
+        this_payoff = this_spot - strike
+        #Value of option is zero is our price is less than the strike
+        this_payoff = this_payoff if this_payoff > 0 else 0
+        running_sum_call_payoff+=this_payoff
+    average_payoff = running_sum_call_payoff/num_traces
+    return round(average_payoff,2)
+
+estimated_call_option_price = genCauchyCallOptionPrice(2,3100,3000,0,0.02,1000)
+print(estimated_call_option_price)
