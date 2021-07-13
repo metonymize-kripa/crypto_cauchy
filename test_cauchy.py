2044d80c8a31d103004c140ef85920a6f7f635c5
Harmeet <goindi@gmail.com>
2021-07-13 19:57:38 -0400

    t distribution

--- test_cauchy.py
+++ test_cauchy.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import scipy
-from scipy.stats import cauchy, norm
+from scipy.stats import cauchy, norm, t
 import numpy as np
 import pandas as pd
 
-my_data = pd.read_csv('ETH-USD-samples.csv', delimiter=',')
+my_data = pd.read_csv('BTC-USD-samples.csv', delimiter=',')
 my_data['returns']=np.log(my_data.Close) - np.log(my_data.Close.shift(1))
 my_data.returns[my_data.returns == np.inf] = 0
 my_data.returns[my_data.returns == -np.inf] = 0
@@ -17,13 +17,19 @@ norm_params_fit  = scipy.stats.distributions.norm.fit(my_data.returns)
 mean, var = norm_params_fit
 print("Normal Params Estimated:", norm_params_fit)
 
+t_params_fit  = scipy.stats.distributions.t.fit(my_data.returns)
+t_df, t_loc, t_scale = t_params_fit
+
+print("T Params Estimated:", t_params_fit)
 x = np.linspace(-0.25,0.25,100)
 
 cauchy_fitted_data = scipy.stats.distributions.cauchy.pdf(x, loc, scale)
 norm_fitted_data = scipy.stats.distributions.norm.pdf(x, mean, var)
+t_fitted_data = scipy.stats.distributions.t.pdf(x, t_df, t_loc, t_scale)
 
 plt.hist(my_data.returns, bins=100, range=(-0.25,0.25), density=True)
 plt.plot(x,cauchy_fitted_data,'r-')
 plt.plot(x,norm_fitted_data,'g-')
+plt.plot(x,t_fitted_data,'b-')
 
 plt.show()
--- vanilla_option_pricer.py
+++ vanilla_option_pricer.py
@@ -1,5 +1,5 @@
 import math, random, numpy, scipy, sys
-from scipy.stats import cauchy, norm
+from scipy.stats import cauchy, norm, t
 import matplotlib.pyplot as plt
 
 def genCauchySpotPriceSample(spot,days_ahead,loc,scale):
@@ -7,8 +7,31 @@ def genCauchySpotPriceSample(spot,days_ahead,loc,scale):
     cauchy_returns_trace = cauchy.rvs(loc,scale,days_ahead)
     for cauchy_return_sample in cauchy_returns_trace:
         #print(f'gg-{cauchy_return_sample}')
-        if cauchy_return_sample < 2:
+        if cauchy_return_sample < 1:
             running_sum_spot = running_sum_spot*math.exp(cauchy_return_sample)
+    #print(running_sum_spot)
+    return running_sum_spot
+
+def genNormSpotPriceSample(spot,days_ahead,loc,scale):
+    running_sum_spot=spot
+    norm_returns_trace = norm.rvs(loc,scale,days_ahead)
+    #print(norm_returns_trace)
+    for norm_return_sample in norm_returns_trace:
+        #print(f'gg-{cauchy_return_sample}')
+        #if norm_return_sample < 1:
+        running_sum_spot = running_sum_spot*math.exp(norm_return_sample)
+    #if running_sum_spot <spot:
+    #    print(running_sum_spot)
+    return running_sum_spot
+
+def genStudentTSpotPriceSample(spot,days_ahead,df,loc,scale):
+    running_sum_spot=spot
+    t_returns_trace = t.rvs(df,loc,scale,days_ahead)
+    for t_return_sample in t_returns_trace:
+        #print(f'gg-{cauchy_return_sample}')
+        #if t_return_sample < 1:
+        running_sum_spot = running_sum_spot*math.exp(t_return_sample)
+    #print(running_sum_spot)
     return running_sum_spot
 
 ##Simple Monte Carlo Pricing Vanilla Call Option using Cauchy Distribution, and 0 interest rate
@@ -17,6 +40,33 @@ def genCauchyCallOptionPrice(days_ahead, strike, spot, loc, scale, num_traces):
     ##Simulate spot price traces and corresponding pay offs
     for i in range(num_traces):
         this_spot = genCauchySpotPriceSample(spot,days_ahead,loc,scale)
+        #Determine payoff of this specific path
+        this_payoff = this_spot - strike
+        #Value of option is zero is our price is less than the strike
+        this_payoff = this_payoff if this_payoff > 0 else 0
+        running_sum_call_payoff+=this_payoff
+    average_payoff = running_sum_call_payoff/num_traces
+    return round(average_payoff,2)
+
+def genNormCallOptionPrice(days_ahead, strike, spot, loc, scale, num_traces):
+    running_sum_call_payoff = 0
+    ##Simulate spot price traces and corresponding pay offs
+    for i in range(num_traces):
+        this_spot = genNormSpotPriceSample(spot,days_ahead,loc,scale)
+        #Determine payoff of this specific path
+        this_payoff = this_spot - strike
+        #Value of option is zero is our price is less than the strike
+        this_payoff = this_payoff if this_payoff > 0 else 0
+        running_sum_call_payoff+=this_payoff
+        
+    average_payoff = running_sum_call_payoff/num_traces
+    return round(average_payoff,2)
+    
+def genTCallOptionPrice(days_ahead, strike, spot,df, loc, scale, num_traces):
+    running_sum_call_payoff = 0
+    ##Simulate spot price traces and corresponding pay offs
+    for i in range(num_traces):
+        this_spot = genStudentTSpotPriceSample(spot,days_ahead,df,loc,scale)
         #Determine payoff of this specific path
         this_payoff = this_spot - strike
         #Value of option is zero is our price is less than the strike
@@ -36,17 +86,59 @@ if __name__== "__main__":
     print(sys.argv)
     strike = float(sys.argv[2])
     spot = float(sys.argv[3])
-    num_traces = 10000
+    num_traces = 100000
     loc = 0
     scale = 0
-    if sys.argv[1].upper() == 'ETH':
-        loc = -0.00012
-        scale = 0.02453
-    if sys.argv[1].upper() == 'BTC':
-        loc = 0.0021
-        scale = 0.01457
-    estimated_call_option_price = genCauchyCallOptionPrice(30,strike,spot,loc,scale,num_traces)
-    print(estimated_call_option_price)
-    generated_samples=[genCauchyCallOptionPrice(30,strike,spot,loc,scale,1000) for _ in range(500)]
-    plt.hist(generated_samples, bins=30, range=(0,30000), density=True)
-    plt.show()
+    ndays = 10
+    df=1
+    if sys.argv[4] == 'T':
+        if sys.argv[1].upper() == 'ETH':
+            df = 2.2810757
+            loc = 0.001038856
+            scale = 0.0337545
+        if sys.argv[1].upper() == 'BTC':
+            df = 2.03146
+            loc = 0.002328
+            scale = 0.01981
+        estimated_call_option_price = genTCallOptionPrice(ndays,strike,spot,df,loc,scale,num_traces)
+        print(estimated_call_option_price)
+        generated_samples=[genTCallOptionPrice(ndays,strike,spot,df,loc,scale,1000) for _ in range(500)]
+        plt.hist(generated_samples, bins=30, range=(0,30000), density=True)
+        plt.show()
+    elif sys.argv[4] == 'C':
+        if sys.argv[1].upper() == 'ETH':
+            loc = -0.00012
+            scale = 0.02453
+        if sys.argv[1].upper() == 'BTC':
+            #using all available data
+            #loc = 0.0021
+            #scale = 0.01457
+            #using data from mid 2017
+            loc = 0.0020628
+            scale = 0.01731177
+        estimated_call_option_price = genCauchyCallOptionPrice(ndays,strike,spot,loc,scale,num_traces)
+        print(estimated_call_option_price)
+        generated_samples=[genCauchyCallOptionPrice(ndays,strike,spot,loc,scale,1000) for _ in range(500)]
+        plt.hist(generated_samples, bins=30, range=(0,30000), density=True)
+        plt.show()
+    elif sys.argv[4] == 'N':
+        if sys.argv[1].upper() == 'ETH':
+            loc = 0.0029975
+            scale = 0.0681606
+        if sys.argv[1].upper() == 'BTC':
+            #using all available data
+            #loc = 0.0021
+            #scale = 0.01457
+            #using data from mid 2017
+            loc = 0.00170646
+            scale = 0.00170646
+        estimated_call_option_price = genNormCallOptionPrice(ndays,strike,spot,loc,scale,num_traces)
+        print(estimated_call_option_price)
+        generated_samples=[genNormCallOptionPrice(ndays,strike,spot,loc,scale,1000) for _ in range(500)]
+        plt.hist(generated_samples, bins=30, range=(0,30000), density=True)
+        plt.show()
+
+    
+    
+    
+    
